6f5202809782318739c1217f1fd33abb412b95ea
anandmatti <anand.matti@gmail.com>
2020-02-06 20:43:21 +0530

    Added solution for Problem 2

--- src/Problem1.kt
+++ src/Problem1.kt
@@ -1,7 +1,11 @@
+package problem1
+
 /*
 Problem Statement:
+
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
+
  */
 fun main(args: Array<String>) {
     val max = 1000
